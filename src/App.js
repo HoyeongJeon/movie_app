2d710b9c05e9b189512273ba66f6be39d46a29d8
전호영 <78394999+HoyeongJeon@users.noreply.github.com>
2021-07-03 22:18:03 +0900

    React Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    isLoading: true,
+    movie: []
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false})
+    }, 3000);
+  }
   render() {
-    return <div />;
+    const { isLoading } = this.state; 
+    return <div>
+      {isLoading ? "Loading..." : "I'm ready"}
+    </div>; // movie를 render하고 map을 만든 후 movie를 render함.
   }
 }
 export default App;
