fb03ea021901e6469860b7c6a3be8ff99b8b097e
전호영 <78394999+HoyeongJeon@users.noreply.github.com>
2021-07-04 00:43:21 +0900

    cssing...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
     isLoading: true,
-    movie: []
-  }
+    movies: [],
+  };
   getMovies = async () => {
-     const moives = await axios.get("https://yts.mx/api/v2/list_movies.json");
-  }
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
   componentDidMount() {
-   this.getMovies();
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state; 
-    return <div>
-      {isLoading ? "Loading..." : "I'm ready"}
-    </div>; // movie를 render하고 map을 만든 후 movie를 render함.
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genre={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    ); // movie를 render하고 map을 만든 후 movie를 render함.
   }
 }
 export default App;
